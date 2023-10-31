b50dc06638df910b8e33612d6ea5ba2a505feccf
daoatzy <dafasetiadi65@gmail.com>
2023-10-31 18:37:19 +0700

    update array

--- src/kompen.java
+++ src/kompen.java
@@ -2,20 +2,33 @@ import java.util.Scanner;
 public class kompen {
     
     public static void main(String[] args) {
-        int alpa, kompensasi;
+        int alpa = 0;
+        int totalAlpa = 0;
+        int kompensasi = 0;
+        int semester;
         boolean kondisi;
-        
         Scanner sc = new Scanner(System.in);
-         System.out.print("masukkan jumlah alpa : ");
-         alpa = sc.nextInt();
+        
+            System.out.println("masukkan jumlah jam pelajaran");
+            int n = sc.nextInt();
+            int [] jampel = new int[n];
+        for (int i = 0; i < jampel.length; i++) {
+            System.out.println("masukkan jam alpa : ");
+            alpa = sc.nextInt();
+            totalAlpa += alpa; 
+        }
          System.out.println("apakah mengerjakan semester ini?");
-         kompensasi = alpa*2;
          kondisi = sc.nextBoolean();
-         if (kondisi == true ) {
-            System.out.println("jumlah kompen anda :" + alpa + " jam");
+         if (!kondisi) {
+           System.out.println("Masukkan semester berapa mengerjakan : ");
+            semester = sc.nextInt();
+            for (int i = 0; i < semester; i++) {
+                kompensasi = totalAlpa*2;
+            }
+             System.out.println("jumlah kompen yang harus ditanggung: "+kompensasi);
          }
          else{
-            System.out.println("jumlah kompen anda :" + kompensasi + " jam");
+          System.out.println("jumlah kompen yang harus ditanggung: "+totalAlpa);
          }
         sc.close();
 
