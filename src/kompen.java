e937c493726b0ac3f7312651db86fa6cb2eda3d2
daoatzy <dafasetiadi65@gmail.com>
2023-10-31 19:37:24 +0700

    fix perhitungan kompen

--- src/kompen.java
+++ src/kompen.java
@@ -4,7 +4,6 @@ public class kompen {
     public static void main(String[] args) {
         int alpa = 0;
         int totalAlpa = 0;
-        int kompensasi = 0;
         int semester;
         boolean kondisi;
         Scanner sc = new Scanner(System.in);
@@ -20,12 +19,15 @@ public class kompen {
          System.out.println("apakah mengerjakan semester ini?");
          kondisi = sc.nextBoolean();
          if (!kondisi) {
-           System.out.println("Masukkan semester berapa mengerjakan : ");
+            if (totalAlpa <=0) {
+                System.out.println("jam alpa harus lebih dari 0");
+            }
+            System.out.println("Masukkan semester berapa mengerjakan : ");
             semester = sc.nextInt();
             for (int i = 0; i < semester; i++) {
-                kompensasi = totalAlpa*2;
+                totalAlpa = totalAlpa*2;
             }
-             System.out.println("jumlah kompen yang harus ditanggung: "+kompensasi);
+             System.out.println("jumlah kompen yang harus ditanggung: "+totalAlpa);
          }
          else{
           System.out.println("jumlah kompen yang harus ditanggung: "+totalAlpa);
